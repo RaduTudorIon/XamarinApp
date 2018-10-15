0ae00d72e06ab6877bfbf6cb11a83fc4044b906c
RaduTudorIon <radutudorion@gmail.com>
2018-10-16 00:35:40 +0300

    Adaugat partea cu json

--- XamarinApp/XamarinApp/Pages/Page3.xaml.cs
+++ XamarinApp/XamarinApp/Pages/Page3.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,31 @@ namespace XamarinApp
 		public Page3 ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            GetRestaurants();
+        }
+
+        public static Command MyButton_Click = new Command((object item) => {
+            var myItem = item as Restaurants;
+            // Do something with myItem...
+        });
+
+        private async void GetRestaurants()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                var response = await client.GetStringAsync("https://api.myjson.com/bins/vf3s0");
+
+                var restaurants = JsonConvert.DeserializeObject<List<Restaurants>>(response);
+
+                ProductsListView.ItemsSource = restaurants;
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+    }
 }
---
+++ XamarinApp/XamarinApp/Restaurants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp
+{
+    public class Restaurants
+    {
+        public int Id { get; set; }
+        public string RestaurantName { get; set; }
+        public string Address { get; set; }
+        public string Website { get; set; }
+    }
+}
